b05bfa1dbcc635d0e2ed8a8ca5786f87cff948d2
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-11-24 21:35:51 +0300

    deployment to heroku

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,8 @@ from . import views
 urlpatterns=[
     url(r'^$',views.news_of_day,name='newsToday'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews') 
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    url(r'^article/(\d+)',views.article,name ='article') 
 ]
 
 if settings.DEBUG:
